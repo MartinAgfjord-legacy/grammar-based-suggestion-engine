785912ea33f5de3f2118a13cdcfe2b334949f3bb
Martin Agfjord <eidelmando@gmail.com>
2014-05-15 18:32:28 +0200

    Removed old grammar

--- Programming/Simple/StringOper.gf
+++
@@ -1,5 +0,0 @@
-    resource StringOper = {
-      oper
-        SS : Type = {s : Str} ;
-        ss : Str -> SS = \x -> {s = x} ;
-    }
--- Programming/Simple/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gf -src --gfo-dir=bin -make  -literal=Symb SimpleEng.gf SimpleSolr.gf ; mv Simple.pgf /tmp
-#gf -src --gfo-dir=bin -make SimpleEng.gf SimpleSolr.gf
