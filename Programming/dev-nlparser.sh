49e01a317bf91bf115ef51987e79c78e7d6e704c
Per Fredelius <per.fredelius@findwise.com>
2014-11-25 19:01:40 +0100

    running jetty debug without suspend

--- Programming/dev-nlparser.sh
+++ Programming/dev-nlparser.sh
@@ -2,7 +2,9 @@
 cd /vagrant/bundle/nlparser
 
 # debug opts
-export MAVEN_OPTS='-Xmx1500m -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044'
+#export MAVEN_OPTS='-Xmx1500m -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044'
+# debug without suspend
+export MAVEN_OPTS='-Xmx1500m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044'
 #export MAVEN_OPTS='-Xmx1500m'
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
